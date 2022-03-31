4fe5c469ba14f9dab3da5be6626bbb5109ed1984
Alex Li <github@alexv525.com>
2022-03-31 15:35:30 +0800

    ✨ Finish [Implement LazyIndexedStack]'s instructions

--- implement_lazy_indexed_stack/introduction/instructions.md
+++ implement_lazy_indexed_stack/introduction/instructions.md
@@ -23,9 +23,26 @@ to get started using Flutter.
   allowfullscreen>
 </iframe>
 
-`IndexedStack` is a `Stack that shows a single child from a list of children.
+`IndexedStack` is a `Stack` that shows a single child from a list of children.
 The displayed child is the one with the given _index_.
 The stack is always as big as the largest child.
 When the _index_ is null, then nothing is displayed.
 
 ## When should I use `IndexedStack`?
+
+Considering you have multiple pages want to place in a same page
+and switch them by the bottom navigation,
+or you have a sequence of widgets that needs to display.
+`IndexedStack` perfectly suit for these cases.
+
+{% comment %}
+A GIF should be presented here.
+{% endcomment %}
+
+## What's next?
+
+Throughout this workshop, we'll build on this code.
+We’ll convert the `IndexedStack` to be a more
+efficient, lazy loading `LazyIndexedStack`.
+This allows us to add save performance cost at the first place.
+Move on to the next step to begin!
---
+++ implement_lazy_indexed_stack/introduction/snippet.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(DemoApp());
+}
+
+class DemoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Demo App',
+      home: const DemoPage(),
+    );
+  }
+}
+
+class DemoPage extends StatefulWidget {
+  const DemoPage({Key? key}) : super(key: key);
+
+  @override
+  State<DemoPage> createState() => _DemoPageState();
+}
+
+class _DemoPageState extends State<DemoPage> {
+  int _index = 0;
+
+  void _selectIndex(int index) {
+    setState(() {
+      _index = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('IndexedStack')),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: IndexedStack(
+              index: _index,
+              children: List<Widget>.generate(
+                3,
+                    (int index) => Center(
+                  child: Text(
+                    'Children index:\n'
+                        '${'$index' * (index + 1)}',
+                    style: Theme.of(context).textTheme.headlineLarge,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          BottomNavigationBar(
+            currentIndex: _index,
+            onTap: _selectIndex,
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.filter_1),
+                label: '1',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.filter_2),
+                label: '2',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.filter_3),
+                label: '3',
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
