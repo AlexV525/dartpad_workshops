7fc38195d241a8543980ffafe527258d67d75052
Alex Li <github@alexv525.com>
2022-04-07 21:37:15 +0800

    ✨ Finish `Completion` step

---
+++ implement_lazy_indexed_stack/completion/instructions.md
@@ -0,0 +1,18 @@
+# Completion
+
+Congratulations! 👍 You have reached the end of the workshop.
+
+In this workshop we
+
+- learned how to use and when to use the `IndexedStack`
+- implemented the `LazyIndexedStack` step by step
+- explored how to improve the state persistence during children updates
+
+Hope you have **MASTERED all the above content**.
+
+To learn more about these widgets, and all the Flutter widgets, you can
+- visit **[flutter.dev](https://flutter.dev/)**
+- dive in to our **[API documentation](https://api.flutter.dev/)**
+- and check out the
+  **[Flutter YouTube channel](https://www.youtube.com/channel/UCwXdFgeE9KYzlDdR7TG9cMw)**
+  for more tutorials & tips on creating your next great user experience.
---
+++ implement_lazy_indexed_stack/completion/snippet.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('LazyIndexedStack')),
+        body: const Builder(builder: buildLazyIndexedStack),
+      ),
+    ),
+  );
+}
+
+Widget buildLazyIndexedStack(BuildContext context) {
+  return LazyIndexedStack(
+    index: 0,
+    alignment: Alignment.center,
+    sizing: StackFit.expand,
+    children: List<Widget>.generate(
+      3,
+      (int index) => Text(
+        'Children index:\n'
+        '${'$index' * (index + 1)}',
+      ),
+    ),
+  );
+}
+
+class LazyIndexedStack extends StatefulWidget {
+  const LazyIndexedStack({
+    Key? key,
+    this.alignment = AlignmentDirectional.topStart,
+    this.textDirection,
+    this.sizing = StackFit.loose,
+    this.index = 0,
+    this.children = const <Widget>[],
+  }) : super(key: key);
+
+  final AlignmentGeometry alignment;
+  final TextDirection? textDirection;
+  final StackFit sizing;
+  final int? index;
+  final List<Widget> children;
+
+  @override
+  State<LazyIndexedStack> createState() => _LazyIndexedStackState();
+}
+
+class _LazyIndexedStackState extends State<LazyIndexedStack> {
+  late final List<bool> _activatedList = List<bool>.generate(
+    widget.children.length,
+    (int i) => i == widget.index,
+  );
+
+  void _activateIndex(int? index) {
+    if (index == null) {
+      return;
+    }
+    if (!_activatedList[index]) {
+      _activatedList[index] = true;
+    }
+  }
+
+  List<Widget> _buildChildren(BuildContext context) {
+    return <Widget>[
+      for (int i = 0; i < widget.children.length; i++)
+        if (_activatedList[i] == true)
+          widget.children[i]
+        else
+          const SizedBox.shrink(),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _activateIndex(widget.index);
+    return IndexedStack(
+      alignment: widget.alignment,
+      textDirection: widget.textDirection,
+      sizing: widget.sizing,
+      index: widget.index,
+      children: _buildChildren(context),
+    );
+  }
+}
