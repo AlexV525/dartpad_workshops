b23c968b0fbc4b9c022ea8f72443c7c33f0eb940
Alex Li <github@alexv525.com>
2022-04-14 16:30:59 +0800

    🚸 Improve interaction experiences

--- implement_lazy_indexed_stack/step_03/solution.dart
+++ implement_lazy_indexed_stack/step_03/solution.dart
@@ -1,29 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('LazyIndexedStack')),
-        body: const Builder(builder: buildLazyIndexedStack),
-      ),
-    ),
-  );
-}
-
-Widget buildLazyIndexedStack(BuildContext context) {
-  return LazyIndexedStack(
-    index: 0,
-    alignment: Alignment.center,
-    sizing: StackFit.expand,
-    children: List<Widget>.generate(
-      3,
-      (int index) => Text(
-        'Children index:\n'
-        '${'$index' * (index + 1)}',
-      ),
-    ),
-  );
+  runApp(const DemoApp());
 }
 
 class LazyIndexedStack extends StatefulWidget {
@@ -72,6 +50,117 @@ class _LazyIndexedStackState extends State<LazyIndexedStack> {
       sizing: widget.sizing,
       index: widget.index,
       children: _buildChildren(context),
+    );
+  }
+}
+
+class DemoApp extends StatelessWidget {
+  const DemoApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(title: 'LazyIndexedStack', home: DemoPage());
+  }
+}
+
+class DemoPage extends StatefulWidget {
+  const DemoPage({Key? key}) : super(key: key);
+
+  @override
+  State<DemoPage> createState() => _DemoPageState();
+}
+
+class _DemoPageState extends State<DemoPage> {
+  int _index = 0;
+
+  void _selectIndex(int index) {
+    setState(() {
+      _index = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('IndexedStack')),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: LazyIndexedStack(
+              index: _index,
+              children: List<Widget>.generate(
+                3,
+                (int index) => _SubIndexPage(index),
+              ),
+            ),
+          ),
+          BottomNavigationBar(
+            currentIndex: _index,
+            onTap: _selectIndex,
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.filter_1),
+                label: '1',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.filter_2),
+                label: '2',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.filter_3),
+                label: '3',
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _SubIndexPage extends StatefulWidget {
+  const _SubIndexPage(this.index, {Key? key}) : super(key: key);
+
+  final int index;
+
+  @override
+  State<_SubIndexPage> createState() => _SubIndexPageState();
+}
+
+class _SubIndexPageState extends State<_SubIndexPage> {
+  DateTime? _displayTime;
+
+  @override
+  void initState() {
+    super.initState();
+    Future<void>.delayed(const Duration(milliseconds: 300), () {
+      if (mounted) {
+        setState(() {
+          _displayTime = DateTime.now().subtract(
+            const Duration(milliseconds: 300),
+          );
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        const Spacer(),
+        Text(
+          'This is page ${widget.index}',
+          style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                fontSize: 64,
+              ),
+          textAlign: TextAlign.center,
+        ),
+        if (_displayTime == null)
+          const Spacer()
+        else
+          Expanded(child: Text('initState() ran at $_displayTime')),
+      ],
     );
   }
 }
